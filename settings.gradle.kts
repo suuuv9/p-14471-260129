bbde40a144e3a5e37d52b0f93b6bf70b756ddb74
suuuu9 <asb040923@gmail.com>
2026-02-04 22:39:37 +0900

    feat: 프로젝트 초기 설정 및 입력 처리

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "p-14471-260129"
---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,16 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        sc.nextLine(); //입력값 받기
+
+        System.out.println("종료");
+    }
+}
