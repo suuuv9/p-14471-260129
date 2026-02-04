4a685f9da5ab9492d2d8da7ef7d65990a13fe3e3
suuuu9 <asb040923@gmail.com>
2026-02-05 01:11:27 +0900

    2단계 명령어 처리 구현

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -8,9 +8,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        sc.nextLine(); //입력값 받기
 
-        System.out.println("종료");
+        while(true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine(); //cmd라는 변수에 입력한 값이 들어오도록 함
+
+            if(cmd.equals("종료")) { //숫자는 ==로 비교하지만, 문자는 .equals로 비교!
+                break;
+            }
+
+            if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSaying = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+            }
+        }
     }
 }
