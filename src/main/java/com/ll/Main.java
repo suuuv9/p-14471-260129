35ec0fc84b2c3b5cf550b74907248630250b3037
suuuu9 <asb040923@gmail.com>
2026-02-06 02:05:42 +0900

    3단계 등록시 생성된 명언번호 노출

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -13,15 +13,15 @@ public class Main {
             System.out.print("명령) ");
             String cmd = sc.nextLine(); //cmd라는 변수에 입력한 값이 들어오도록 함
 
-            if(cmd.equals("종료")) { //숫자는 ==로 비교하지만, 문자는 .equals로 비교!
-                break;
-            }
-
             if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String wiseSaying = sc.nextLine();
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
+                System.out.println("1번 명언이 등록되었습니다.");
+            }
+            else if(cmd.equals("종료")) { //숫자는 ==로 비교하지만, 문자는 .equals로 비교!
+                break;
             }
         }
     }
